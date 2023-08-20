50ef848773e2a843dff11dc3609b02ecfc6e0c44
Anton <antonkrivonogov081004@yandex.ru>
2023-08-20 22:54:21 +0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let number = String(n).split('');
+
+    if ( number[0] == '-') {
+        number[0] = ' ';
+        n = +number.join('');
+
+        return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+    } else {
+        return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+    }
+
+    
+
 }
+
